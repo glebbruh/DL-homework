380ba15dca3fb4c553f157e4c9adbbbbd3e6c2ec
Gleb Poletaev <poletaev.gl@gmail.com>
2026-08-01 04:36:29 +0300

    Add cross entropy loss

---
+++ src/loss/cross_entropy.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn
+
+
+class CrossEntropyLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.loss = nn.CrossEntropyLoss()
+
+    def forward(
+        self,
+        logits: torch.Tensor,
+        labels: torch.Tensor,
+        **batch,
+    ):
+        return {
+            "loss": self.loss(logits, labels),
+        }
